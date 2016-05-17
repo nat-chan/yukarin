423fbf821fdd17b5a877cdbe32b1fd2d3048e20d
nat-chan <qzrp01@gmail.com>
2016-05-18 04:08:08 +0900

    5/18

--- crop.sh
+++ crop.sh
@@ -1,2 +1,14 @@
 #!/bin/zsh
-convert $1 txt:-|sed -e "s/: (/,/g" -e "s/).*//g" -e '1d'
+
+#刻み幅
+#横:32
+#縦:48
+
+for y in 4;do
+	mkdir $y
+	cd $y
+	for x in {1..8};do
+		convert -crop 24x38+$((32*x-27-1))+$((48*y-36-2)) ../3657666.png ${x}.png
+	done
+	cd ..
+done
